16ad2a226f836591be1440564917118edffa3aaa
YoJu310 <singhyojna882@gmail.com>
2026-07-14 13:46:44 +0530

    feat: add login and signup page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./../src/pages/Home.jsx"
 import Navbar from "./../src/components/navbar.jsx"
 import { Route,Routes } from 'react-router-dom'
 import Footer from './components/Footer.jsx'
+import Login from './components/Login.jsx'
 import { AppContext } from './contexts/AppContexts.jsx'
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
@@ -17,6 +18,7 @@ const App = () => {
     min-h-screen bg-linear-to-b from-rose-50 via-white to-orange-50">
       <ToastContainer position="bottom-right" />
       <Navbar />
+      {showLogin && <Login />}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
